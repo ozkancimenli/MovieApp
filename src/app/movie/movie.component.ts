7b6447ad609d472151a15274555cb536c6a3ffad
Özkan Çimenli <76163736+ozkancimenli@users.noreply.github.com>
2023-08-27 21:53:42 +0300

    Lesson 2: Generating New Component Using CLI

---
+++ src/app/movie/movie.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'movie',
+  templateUrl: './movie.component.html',
+  styleUrls: ['./movie.component.css']
+})
+export class MovieComponent {
+
+}
