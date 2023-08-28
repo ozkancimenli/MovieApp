0ca02153b04f08ca2c32fdecd379a5c3902ee502
Özkan Çimenli <76163736+ozkancimenli@users.noreply.github.com>
2023-08-28 04:56:55 +0300

    Lesson 6: Event Binding

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { Movies } from "../movie.datasource";
+import { Movie } from "../movie";
 
 @Component({
     selector: 'movies'  ,
@@ -8,7 +9,11 @@ import { Movies } from "../movie.datasource";
 })
 export class MoviesComponent {
 title= 'Movie List';
-
 movies= Movies;
+    selectedMovie: Movie;
+
+onSelect(movie:Movie): void {
+this.selectedMovie = movie;
+}
 
 }
