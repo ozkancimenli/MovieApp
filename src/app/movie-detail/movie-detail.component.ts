dda28279d74867277f3f78a1f2d7e6d7a1901a76
Özkan Çimenli <76163736+ozkancimenli@users.noreply.github.com>
2023-08-28 05:11:05 +0300

    Lesson 7: Input Properties

---
+++ src/app/movie-detail/movie-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Movie } from '../movie';
+
+@Component({
+  selector: 'movie-detail',
+  templateUrl: './movie-detail.component.html',
+  styleUrls: ['./movie-detail.component.css']
+})
+export class MovieDetailComponent {
+@Input () movie:Movie
+}
