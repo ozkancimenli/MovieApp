dcff31aca9df17c29a78b7f83de00c43f104f6f2
Özkan Çimenli <76163736+ozkancimenli@users.noreply.github.com>
2023-08-28 06:15:18 +0300

    Lesson 8: Services

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,7 +1,6 @@
 import { Component } from "@angular/core";
-import { Movies } from "../movie.datasource";
 import { Movie } from "../movie";
-
+import { MovieService } from "../movie.service";
 @Component({
     selector: 'movies'  ,
     templateUrl:'movies.component.html',
@@ -9,11 +8,23 @@ import { Movie } from "../movie";
 })
 export class MoviesComponent {
 title= 'Movie List';
-movies= Movies;
-    selectedMovie: Movie;
+movies: Movie[];
+selectedMovie: Movie;
+
+constructor(private movieService:MovieService){}
+
+ngOnInit():void{
+    this.getMovies();
+}
+
+
 
 onSelect(movie:Movie): void {
 this.selectedMovie = movie;
+}
+
+getMovies():void {
+    this.movies= this.movieService.getMovies();
 }
 
 }
