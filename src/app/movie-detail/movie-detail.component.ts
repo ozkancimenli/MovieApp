07cfb077134e5362110138dc916d9878a240380c
Özkan Çimenli <76163736+ozkancimenli@users.noreply.github.com>
2023-08-29 04:14:02 +0300

    Lesson 13: Movie Details

--- src/app/movie-detail/movie-detail.component.ts
+++ src/app/movie-detail/movie-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Movie } from '../movie';
+import { MovieService } from '../movie.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'movie-detail',
@@ -7,5 +9,21 @@ import { Movie } from '../movie';
   styleUrls: ['./movie-detail.component.css']
 })
 export class MovieDetailComponent {
-@Input () movie:Movie
+  //localhost:4200/detail/2
+  @Input() movie: Movie
+
+  constructor(
+    private movieService: MovieService,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    this.getMovie();
+  }
+
+  getMovie(): void {
+    const id =  this.route.snapshot.paramMap.get('id');
+    this.movieService.getMovie(id)
+          .subscribe(movie=>this.movie === movie);
+  }
 }
--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Movie } from './movie';
 import { Movies } from "./movie.datasource";
-import { Observable, of } from "rxjs";
+import { Observable, of, } from "rxjs";
 import { LoggingService } from './logging.service';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -12,7 +13,11 @@ export class MovieService {
 
   getMovies(): Observable <Movie[]>{
     this.loggingService.add('MovieService: listing movies');
-    
     return of(Movies);
+  }
+
+  getMovie(id: string | number | null): Observable<Movie | undefined>{
+    this.loggingService.add('MovieService: get detail by id='+id)
+    return of(Movies.find(movie=> movie.id === id));
   }
 }
